34d429a028afe6c8e0c0ef0d4c43d14ea391912d
Martinkirke1 <martinkirke1@gmail.com>
2016-10-19 11:01:20 -0600

    test

---
+++ StudentPutApi/StudentController.swift
@@ -0,0 +1,23 @@
+//
+//  StudentController.swift
+//  StudentPutApi
+//
+//  Created by Martin Kirke on 10/19/16.
+//  Copyright © 2016 Ghost. All rights reserved.
+//
+
+import Foundation
+
+class StudentController {
+    
+    /*
+ work:
+    -endpoints
+    -function for getting student
+    -function for adding a student
+    */
+    
+    // properties
+    
+    static let baseURL = URL(string: "")
+}
